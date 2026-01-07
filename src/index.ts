d3fb23e69b82374de05623778ad810a830bf6342
markbakosss <markbakosss@gmail.com>
2026-01-08 00:03:05 +0100

    refactor index to use store

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { fetchRandom } from "./core/words";
 import { checkUrl } from "./core/checkUrl";
-import { writeFile } from "./utils/write";
+import {createStore} from "./core/store";
 
 async function main() {
+
+    const store = await createStore("results.txt");
+
     while (true) {
         const words = await fetchRandom(250);
         for (let i = 0; i < words.length; i++) {
             const status = await checkUrl({raw: words[i]});
             if (status === "available") {
-                writeFile(words[i]);
+                await store.addIfNew(words[i]);
                 console.log(words[i]);
             }
             if (status === "rate_limited") {
